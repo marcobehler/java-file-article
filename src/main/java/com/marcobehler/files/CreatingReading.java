10325f250bd31cc316ff925a6f2a6c5b1aa560fb
marco <marco@marcobehler.com>
2020-05-20 10:47:48 +0200

    initial commit

---
+++ src/main/java/com/marcobehler/files/CreatingReading.java
@@ -0,0 +1,43 @@
+package com.marcobehler.files;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreatingReading {
+
+    public static void main(String[] args) throws URISyntaxException {
+
+        // Java11+
+
+        Path path = Path.of("c:\\dev\\licenses\\windows\\readme.txt");
+        System.out.println(path);
+
+        path = Path.of("c:/dev/licenses/windows/readme.txt");
+        System.out.println(path);
+
+        path = Path.of("c:" , "dev", "licenses", "windows", "readme.txt");
+        System.out.println(path);
+
+        path = Path.of("c:" , "dev", "licenses", "windows").resolve("readme.txt"); // resolve == getChild()
+        System.out.println(path);
+
+        path = Path.of(new URI("file:///c:/dev/licenses/windows/readme.txt"));
+        System.out.println(path);
+
+        // Java < 11
+        path = Paths.get("c:/dev/licenses/windows/readme.txt");
+        System.out.println(path);
+
+        // etc
+    }
+
+/*
+    c:\dev\licenses\windows\readme.txt
+    c:\dev\licenses\windows\readme.txt
+    c:\dev\licenses\windows\readme.txt
+    c:\dev\licenses\windows\readme.txt
+    c:\dev\licenses\windows\readme.txt*/
+
+}
